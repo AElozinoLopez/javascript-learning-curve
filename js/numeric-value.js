4bf8540e42ee2342995b8e6bfc6ab72353db45f4
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2022-07-12 07:53:29 +0100

    Added and commented out some css codes

--- js/numeric-value.js
+++ js/numeric-value.js
@@ -5,7 +5,10 @@ var total;         // Variable declaration
 
 price = 894;                     //Variable assignment
 quantity = 15;                  //Variable assignment
-total = price * quantity;       //Variable assignment
+total = price * quantity;       // Calculate the total by multiplying the price by quantity.
 
+// Get the element with an id of cost.
 var el = document.getElementById('cost');  
 el.textContent = '$' + total;
+
+// Note: textContent does not work in IE8 or earlier
