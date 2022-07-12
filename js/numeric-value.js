9ecc38774c23ef8f6eb41200ee11144e67be48cf
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2022-07-12 07:24:22 +0100

    Added comments to script

--- js/numeric-value.js
+++ js/numeric-value.js
@@ -1,10 +1,11 @@
-var price;
-var quantity;
-var total;
+// Script to use a variable to store a number
+var price;        // Variable declaration
+var quantity;      // Variable declaration
+var total;         // Variable declaration
 
-price = 894;
-quantity = 15;
-total = price * quantity;
+price = 894;                     //Variable assignment
+quantity = 15;                  //Variable assignment
+total = price * quantity;       //Variable assignment
 
-var el = document.getElementById('cost');
+var el = document.getElementById('cost');  
 el.textContent = '$' + total;
