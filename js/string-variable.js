9a96f95c3d225dfa6f6c1384dc79611dd84be517
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2022-07-13 06:44:04 +0100

    Script using a variable to store a string was added.

---
+++ js/string-variable.js
@@ -0,0 +1,11 @@
+// Using A Variable To Store A String
+
+var username;
+var message;
+username = 'Elozino';
+message = 'See my upcoming blog';
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+var elNote = document.getElementById('note');
+elNote.textContent = message;
