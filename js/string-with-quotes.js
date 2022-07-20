a8f83ee44c2090f5a1a13ab936b731db0026bdae
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2022-07-20 08:11:57 +0100

    Added content fo r Elozino Stores

---
+++ js/string-with-quotes.js
@@ -0,0 +1,9 @@
+var titles;
+var message;
+titles = "Elozino's Special Offer";
+message = '<a href=\"index.html\">25% off!</a>';
+
+var elTitle = document.getElementById('titles');
+elTitle.innerHTML = titles;
+var elNote = document.getElementById('notes');
+elNote.innerHTML = message;
